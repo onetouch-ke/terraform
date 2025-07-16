4bcec5b0298dbb9de36f208e9be2f4e471d062f9
AquaRiva <hmk0676@naver.com>
2025-07-16 15:22:57 +0900

    App Test

--- Terraform-App/02-Helm-K8S-Provider.tf
+++ Terraform-App/02-Helm-K8S-Provider.tf
@@ -1,17 +1,25 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+data "aws_eks_cluster" "MSA_eks_cluster" {
+  name = "MSA_eks_cluster"   # 실제 Infra에서 만든 클러스터 이름과 맞춰야 함
+}
+
 data "aws_eks_cluster_auth" "auth" {
-  name = aws_eks_cluster.MSA_eks_cluster.name
+  name = data.aws_eks_cluster.MSA_eks_cluster.name
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.MSA_eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.MSA_eks_cluster.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.MSA_eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.MSA_eks_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.auth.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.MSA_eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.MSA_eks_cluster.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.MSA_eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.MSA_eks_cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.auth.token
   }
 }
