780f3e78703972d1be4edcd184bbc0bc89ef28b4
AquaRiva <hmk0676@naver.com>
2025-07-16 14:46:17 +0900

    Infra App Split

---
+++ Terraform-Infra/01-Provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0.0, <2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+     kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.11.0, < 3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2" # Asia Pacific (Seoul)
+}
