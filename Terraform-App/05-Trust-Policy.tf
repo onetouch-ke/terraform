4394f83e606607eff405d361071d2c4c5331a11d
hojun217 <hojun547@gmail.com>
2025-07-16 17:28:13 +0900

    test2

--- Terraform-App/05-Trust-Policy.tf
+++ Terraform-App/05-Trust-Policy.tf
@@ -1,23 +1,22 @@
-# 2. Trust Policy 기반 Role 생성 (IRSA 용)
+# ALB Controller용 IRSA Role
 resource "aws_iam_role" "alb_controller" {
   name = "alb-controller-irsa-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "${aws_iam_openid_connect_provider.oidc.arn}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.oidc.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(data.aws_eks_cluster.MSA_eks_cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
         }
       }
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
