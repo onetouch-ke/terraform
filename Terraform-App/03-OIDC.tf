88f8c87f2a5e16f9ca0caf7508bb64260508c51d
hojun217 <hojun547@gmail.com>
2025-07-16 17:22:00 +0900

    test1

--- Terraform-App/03-OIDC.tf
+++ Terraform-App/03-OIDC.tf
@@ -1,7 +1,34 @@
-# OIDC Provider
+# 클러스터 참조
+data "aws_eks_cluster" "MSA_eks_cluster" {
+  name = "MSA-cluster" # 클러스터 이름에 맞게 수정
+}
+
+# OIDC Provider 정의
 resource "aws_iam_openid_connect_provider" "oidc" {
   client_id_list  = ["sts.amazonaws.com"]
   url             = data.aws_eks_cluster.MSA_eks_cluster.identity[0].oidc[0].issuer
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0ecd01e10"] # 실제 값 확인
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0ecd01e10"] # 서울 리전 기본값
 }
 
+# ALB Controller용 IRSA Role
+resource "aws_iam_role" "alb_controller" {
+  name = "alb-controller-irsa-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.oidc.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(data.aws_eks_cluster.MSA_eks_cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+}
---
+++ Terraform-App/04-iam-role-alb.tf
@@ -0,0 +1,4 @@
+resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
