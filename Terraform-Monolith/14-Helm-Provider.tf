d28ea3444a9d1ae1ff481660b14ecf082a7df068
AquaRiva <hmk0676@naver.com>
2025-07-16 14:10:29 +0900

    MSA-Test

--- Terraform-Monolith/14-Helm-Provider.tf
+++ Terraform-Monolith/14-Helm-Provider.tf
@@ -7,7 +7,7 @@ data "aws_eks_cluster_auth" "auth" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = aws_eks_cluster.MSA_eks_cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.MSA_eks_cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.auth.token
