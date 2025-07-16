f6d8c943238b6143bb08c6481550609c93f796d0
hojun217 <hojun547@gmail.com>
2025-07-16 17:55:43 +0900

    test5

--- Terraform-App/10-ArgoCD.tf
+++ Terraform-App/10-ArgoCD.tf
@@ -1,20 +1,37 @@
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "argocd"
+  name             = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
   create_namespace = true
 
-  set {
-    name  = "server.extraArgs"
-    value = "{--insecure}"
-  }
+  # ArgoCD Server Insecure 설정
   set {
     name  = "server.insecure"
     value = "true"
   }
+
+  # 서버 실행 시 --insecure 인자 추가 (선택)
+  set {
+    name  = "server.extraArgs"
+    value = "{--insecure}"
+  }
+
+  # LoadBalancer로 외부 노출
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  # Helm Chart에서 CRD 자동 설치
   set {
     name  = "installCRDs"
+    value = "true"
+  }
+
+  # configs.params 설정 방식으로 insecure 설정 (선택)
+  set {
+    name  = "configs.params.server.insecure"
     value = "true"
   }
 }
