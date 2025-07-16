da535a21c20ccde594486dbf936776b8544c0f18
AquaRiva <hmk0676@naver.com>
2025-07-16 14:22:01 +0900

    MSA-Test

--- Terraform-Monolith/14-Kubernetes-Provider.tf
+++ Terraform-Monolith/14-Kubernetes-Provider.tf
@@ -1,3 +1,11 @@
+data "aws_eks_cluster" "main" {
+  name = aws_eks_cluster.MSA_eks_cluster.name
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.MSA_eks_cluster.name
+}
+
 provider "kubernetes" {
   host                   = aws_eks_cluster.MSA_eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.MSA_eks_cluster.certificate_authority[0].data)
