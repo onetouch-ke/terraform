c1ca1ce870151ad80b66a8f6145159f955cd066b
hojun217 <hojun547@gmail.com>
2025-07-17 17:50:59 +0900

    test27

--- Terraform-App/06-attach.tf
+++ Terraform-App/06-attach.tf
@@ -1,5 +1,4 @@
-# 3. Policy를 Role에 Attach
-#resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
-#  role       = aws_iam_role.alb_controller.name
-#  policy_arn = aws_iam_policy.alb_controller.arn
-#}
+resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
