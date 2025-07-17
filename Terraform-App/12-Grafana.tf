733fe04462ee8235447c98843652a69b5eab1b09
hojun217 <hojun547@gmail.com>
2025-07-17 18:45:56 +0900

    test32

--- Terraform-App/12-Grafana.tf
+++ Terraform-App/12-Grafana.tf
@@ -3,5 +3,5 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
   namespace  = "monitoring"
-  depends_on = [helm_release.grafana]
+  depends_on = [helm_release.prometheus]
 }
--- Terraform-App/13-Ingress.tf
+++ Terraform-App/13-Ingress.tf
@@ -2,4 +2,6 @@ resource "helm_release" "ingress" {
   name       = "ingress"
   chart      = "${path.module}/../ingress"
   namespace  = "default"
+
+  depends_on = [helm_release.alb_controller]
 }
