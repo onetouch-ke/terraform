9a568ad3f45e0e642345b02304338eae79572a0a
hojun217 <hojun547@gmail.com>
2025-07-17 17:59:56 +0900

    test28

---
+++ Terraform-App/11-prometheus.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  namespace  = "monitoring"
+
+  set {
+    name  = "server.persistentVolume.storageClass"
+    value = "ebs-sc"
+  }
+
+  depends_on = [kubernetes_namespace.monitoring]
+}
