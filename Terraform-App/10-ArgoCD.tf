e1b85ce23f8324079349bc58777cf3753676a5ed
hojun217 <hojun547@gmail.com>
2025-07-17 19:17:58 +0900

    test34

--- Terraform-App/10-ArgoCD.tf
+++ Terraform-App/10-ArgoCD.tf
@@ -1,23 +1,21 @@
 resource "helm_release" "argocd" {
-  name             = "argo-cd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  namespace        = "argocd"
-  depends_on = [kubernetes_namespace.argocd]
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
 
   set {
     name  = "server.extraArgs"
     value = "{--insecure}"
   }
-
+  set {
+    name  = "server.insecure"
+    value = "true"
+  }
   set {
     name  = "installCRDs"
     value = "true"
   }
 
-  set {
-    name  = "server.insecure"
-    value = "true"
-  }
-
+  depends_on = [kubernetes_namespace.argocd]
 }
