e91f8167d22f81639cd7cbc735ee422eddd0103e
hojun217 <hojun547@gmail.com>
2025-07-17 12:01:17 +0900

    test18

--- Terraform-App/11-Prometheus.tf
+++ Terraform-App/11-Prometheus.tf
@@ -3,5 +3,6 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
   namespace  = "monitoring"
+  version    = "25.21.0"
   depends_on = [kubernetes_namespace.monitoring]
 }
