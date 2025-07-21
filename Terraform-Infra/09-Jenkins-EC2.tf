795c77addcf640c3989eec1680044b6a14218727
AquaRiva <hmk0676@naver.com>
2025-07-21 15:09:23 +0900

    jenkins update

--- Terraform-Infra/09-Jenkins-EC2.tf
+++ Terraform-Infra/09-Jenkins-EC2.tf
@@ -24,7 +24,7 @@ resource "aws_iam_instance_profile" "jenkins_profile" {
 }
 
 resource "aws_instance" "MSA_pub_ec2_jenkins_2a" {
-  ami                         = "ami-0ff0bbc5968fcbc61"
+  ami                         = "ami-056a29f2eddc40520"
   instance_type               = "t3.medium"
   vpc_security_group_ids      = [aws_security_group.MSA_sg_bastion.id]
   subnet_id                   = aws_subnet.MSA_pub_subnet_2a.id
@@ -34,48 +34,24 @@ resource "aws_instance" "MSA_pub_ec2_jenkins_2a" {
 
   user_data = <<-EOF
               #!/bin/bash
-              export DEBIAN_FRONTEND=noninteractive
+              apt update -y
 
-              apt-get update -y
-              apt-get install -y unzip curl ca-certificates gnupg software-properties-common apt-transport-https lsb-release
+              apt install java-17-amazon-corretto -y
 
-              # AWS CLI 설치
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-              unzip awscliv2.zip
-              ./aws/install
+              curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+                /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-              # kubectl 설치
-              curl -LO "https://dl.k8s.io/release/v1.30.1/bin/linux/amd64/kubectl"
-              chmod +x kubectl
-              mv kubectl /usr/local/bin/
+              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+                https://pkg.jenkins.io/debian binary/ | sudo tee \
+                /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-              # Helm 설치
-              curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+              apt install jenkins -y  
 
-              # Kubeconfig 연결
-              /usr/local/bin/aws eks update-kubeconfig \
-                --region ap-northeast-2 \
-                --name ${aws_eks_cluster.MSA_eks_cluster.name} \
-                --kubeconfig /home/ubuntu/.kube/config
-              
-              chown ubuntu:ubuntu /home/ubuntu/.kube/config
+              systemctl enable jenkins
+              systemctl start jenkins
 
-              # cert-manager CRD 설치
-              kubectl create -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.5/cert-manager.crds.yaml \
-                --kubeconfig /home/ubuntu/.kube/config
+              apt -y install git
 
-              # alb-controller CRD 설치
-              kubectl create -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.7.1/config/crd/bases/elbv2.k8s.aws_targetgroupbindings.yaml \
-                --kubeconfig /home/ubuntu/.kube/config
-
-              kubectl create -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.7.1/config/crd/bases/elbv2.k8s.aws_ingressclassparams.yaml \
-                --kubeconfig /home/ubuntu/.kube/config
-
-              # 실행 결과 로그 저장
-              echo "---- kubectl ----" > /home/ubuntu/setup.log
-              kubectl version --client >> /home/ubuntu/setup.log 2>&1
-              echo "---- helm ----" >> /home/ubuntu/setup.log
-              helm version >> /home/ubuntu/setup.log 2>&1
               EOF
 
   root_block_device {
