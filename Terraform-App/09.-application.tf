6ad30927f69ae58a4214ade29262b1f2b708226e
AquaRiva <hmk0676@naver.com>
2025-07-21 19:19:34 +0900

    variable update

--- Terraform-App/09.-application.tf
+++ Terraform-App/09.-application.tf
@@ -8,10 +8,49 @@ resource "helm_release" "msa_boards" {
   name       = "msa-boards"
   chart      = "${path.module}/../mychart/boards"
   namespace  = "default"
+
+  set {
+    name  = "mariadb.userName"
+    value = var.rds_username
+  }
+
+  set {
+    name  = "mariadb.rootPassword"
+    value = var.rds_password
+  }
 }
 
 resource "helm_release" "msa_users" {
   name       = "msa-users"
   chart      = "${path.module}/../mychart/users"
   namespace  = "default"
+
+  set {
+    name  = "mariadb.userName"
+    value = var.rds_username
+  }
+
+  set {
+    name  = "mariadb.rootPassword"
+    value = var.rds_password
+  }
+}
+
+# 변수 선언
+variable "rds_password" {
+  description = "Password for the MariaDB RDS root user"
+  type        = string
+  sensitive   = true
+}
+
+variable "private_key" {
+  description = "SSH private key to connect to bastion"
+  type        = string
+  sensitive   = true
+}
+
+variable "rds_username" {
+  description = "Database admin username"
+  type        = string
+  default     = "MSA_User"  # 또는 원하는 기본값
 }
